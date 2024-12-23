afe87ecd9d849ca96074f609fe87a70cc1e2cb4d
Gökhan Genç <64269660+GGNC@users.noreply.github.com>
2024-12-23 21:27:46 +0300

    chore: update Tailwind config and global CSS properties

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">
+    <div className="flex-center h-screen">
+      <h1 className="h1">
         StoreIt - The onlt storage solution you'll ever need
       </h1>
     </div>
