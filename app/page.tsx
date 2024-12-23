eee8bac08d5ac881ee5afe8b514723b1d43d8358
Gökhan Genç <64269660+GGNC@users.noreply.github.com>
2024-12-23 21:17:14 +0300

    feat: initialize Next.js project with default settings

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl">
+        StoreIt - The onlt storage solution you'll ever need
+      </h1>
+    </div>
+  );
+}
