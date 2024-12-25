ac62d0005ef20e4b3f6d5c13270ec92a9dcbf1d5
Gökhan Genç <64269660+GGNC@users.noreply.github.com>
2024-12-25 14:39:41 +0300

    feat(storage) : add file upload functionality

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,7 +1,120 @@
-import React from "react";
+"use client";
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { Button } from "./ui/button";
+import { cn, convertFileToUrl, getFileType } from "@/lib/utils";
+import Image from "next/image";
+import Thumbnail from "./Thumbnail";
+import { MAX_FILE_SIZE } from "@/config/constants";
+import { useToast } from "@/hooks/use-toast";
+import { uploadFile } from "@/lib/actions/file.actions";
+import { usePathname } from "next/navigation";
 
-function FileUploader() {
-  return <div>FileUploader</div>;
+interface FileUploaderProps {
+  ownerId: string;
+  accountId: string;
+  className?: string;
+}
+
+function FileUploader({ ownerId, accountId, className }: FileUploaderProps) {
+  const path = usePathname();
+  const { toast } = useToast();
+  const [files, setFiles] = useState<File[]>([]);
+  const uploadingFiles = files.map((file, index) => {
+    const { type, extension } = getFileType(file.name);
+    const handleRemoveFile = (
+      event: React.MouseEvent<HTMLImageElement, MouseEvent>,
+      fileName: string
+    ) => {
+      event.stopPropagation();
+      setFiles((prevFiles) =>
+        prevFiles.filter((file) => file.name !== fileName)
+      );
+    };
+    return (
+      <li key={`${file.name}-${index}`} className="uploader-preview-item">
+        <div className="flex items-center gap-3">
+          <Thumbnail
+            type={type}
+            extention={extension}
+            url={convertFileToUrl(file)}
+          />
+          <div className="preview-item-name">
+            {file.name}
+            <Image
+              src="/assets/icons/file-loader.gif"
+              alt="loader"
+              width={80}
+              height={26}
+            />
+          </div>
+        </div>
+        <Image
+          src="/assets/icons/remove.svg"
+          alt="remove"
+          width={24}
+          height={24}
+          onClick={(event) => handleRemoveFile(event, file.name)}
+        />
+      </li>
+    );
+  });
+
+  const onDrop = useCallback(
+    async (acceptedFiles: File[]) => {
+      setFiles(acceptedFiles);
+      const uploadPromises = acceptedFiles.map(async (file) => {
+        if (file.size > MAX_FILE_SIZE) {
+          setFiles((prevFiles) =>
+            prevFiles.filter((f) => f.name !== file.name)
+          );
+          return toast({
+            description: (
+              <p className="body-2 text-white">
+                <span className="font-semibold">{file.name}</span> is too large.
+                Max file size is 50MB.
+              </p>
+            ),
+            className: "error-toast",
+          });
+        }
+
+        return uploadFile({ file, ownerId, accountId, path }).then(
+          (uploadedFile) => {
+            if (uploadedFile) {
+              setFiles((prevFiles) =>
+                prevFiles.filter((f) => f.name !== uploadedFile.name)
+              );
+            }
+          }
+        );
+      });
+      await Promise.all(uploadPromises);
+    },
+    [ownerId, accountId, path]
+  );
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()} className="curosr-pointer">
+      <input {...getInputProps()} />
+      <Button type="button" className={cn("uploader-button", className)}>
+        <Image
+          src="/assets/icons/upload.svg"
+          width={24}
+          height={24}
+          alt="upload"
+        />
+        <p>Upload</p>
+      </Button>
+      {files.length > 0 && (
+        <ul className="uploader-preview-list">
+          <h4 className="h4 text-light-100">Uploading</h4>
+          {uploadingFiles}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default FileUploader;
--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -18,14 +18,14 @@ import { Button } from "./ui/button";
 import FileUploader from "./FileUploader";
 import { signOutUser } from "@/lib/actions/user.actions";
 interface MobileNavigationProps {
-  ownerId: string;
+  $id: string;
   accountId: string;
   fullName: string;
   avatar: string;
   email: string;
 }
 function MobileNavigation({
-  ownerId,
+  $id: ownerId,
   accountId,
   fullName,
   avatar,
@@ -96,7 +96,7 @@ function MobileNavigation({
             </nav>
             <Separator className="my-5 bg-light-100/20" />
             <div className="flex flex-col justify-between gap-5 pb-5">
-              <FileUploader />
+              <FileUploader ownerId={ownerId} accountId={accountId} />
               <Button
                 type="submit"
                 className="mobile-sign-out-button"
