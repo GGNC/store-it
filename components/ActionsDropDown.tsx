14315c7bc8ad04ef3a88126ce654855430f5191d
Gökhan Genç <64269660+GGNC@users.noreply.github.com>
2024-12-25 15:25:38 +0300

    feat : add visualization for type pages and file display card component

---
+++ components/ActionsDropDown.tsx
@@ -0,0 +1,8 @@
+import { Models } from "node-appwrite";
+import React from "react";
+
+function ActionsDropDown({ file }: { file: Models.Document }) {
+  return <div>ActionsDropDown</div>;
+}
+
+export default ActionsDropDown;
